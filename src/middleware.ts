c327a7d2b4470eba381f5283f26aa1487a31ecc7
Rasheem07 <mohdrasheem@icloud.com>
2025-05-29 01:21:18 +0530

    fixed types

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,18 +4,18 @@ import { getToken } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function middleware(req: NextRequest) {
-  const cookieStore = await cookies();
-  const accesToken = cookieStore.get("access_token");
-  const refreshToken = cookieStore.get("refresh_token");
+  // const cookieStore = await cookies();
+  // const accesToken = cookieStore.get("access_token");
+  // const refreshToken = cookieStore.get("refresh_token");
 
-  console.log("access token", accesToken);
-  console.log("refresh token", refreshToken);
+  // console.log("access token", accesToken);
+  // console.log("refresh token", refreshToken);
 
-  if (!refreshToken) {
-    return NextResponse.redirect(
-      `http://localhost:3001/auth/signin?redirectURL=${encodeURIComponent(req.url)}`
-    );
-  }
+  // if (!refreshToken) {
+  //   return NextResponse.redirect(
+  //     `http://localhost:3001/auth/signin?redirectURL=${encodeURIComponent(req.url)}`
+  //   );
+  // }
 
   return NextResponse.next();
 }
